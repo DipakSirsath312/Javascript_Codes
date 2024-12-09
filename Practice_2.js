81b83b42e34c77c40c640859da49d9ce651f739b
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-09 13:54:05 +0530

    [ADDED]:Dipak - This program will be just practice object

---
+++ Practice_2.js
@@ -0,0 +1,20 @@
+const profile = {
+    userID : "mr_sirsath_dipak",
+    username : "Dipak Sirsath",
+    Posts : 3,
+    followers : 130,
+    following : 115,
+    follow : true,
+    details : null,
+};
+console.log(profile["userID"]);
+console.log(profile["following"])
+console.log(profile["Posts"]);
+console.log(profile["details"]);
+console.log(profile["true"]);
+
+console.log(typeof profile["followers"]);
+console.log(typeof profile["following"]);
+console.log(typeof profile["follow"]);
+console.log(typeof profile["details"]);
+
