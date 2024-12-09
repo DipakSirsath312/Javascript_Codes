8354f0881b19508563af4df68b832e83846b4cd7
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-09 13:48:29 +0530

    [ADDED]:Dipak - This code operators who can work

---
+++ operators.js
@@ -0,0 +1,25 @@
+// Operators in JS
+// Used to perform some operation on data 
+
+// 1.Airthmetic Operators
+let a = 8;
+let b = 2;
+console.log("a + b =",a + b);
+console.log("a - b =",a - b);
+console.log("a * b =",a * b);
+console.log("a / b =",a / b);
+
+// 2.Modules
+console.log("a % b =",a % b);
+
+// 3.Exponentiation
+console.log("a ** b =",a ** b);
+
+
+// Unary Operators
+// 1.Increment
+a++;
+console.log(a);
+// 2.Decrement
+a--;
+console.log(a);
