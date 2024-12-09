e2dc6e81d57ac54c35dbd5e9d489c40aa95407a2
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-09 13:44:18 +0530

    [ADDED]:Dipak - This program will be javascript keywords

---
+++ keyword.js
@@ -0,0 +1,23 @@
+// let, const & var
+// 1.var keyword
+var Fname = "Dipak Sirsath";
+console.log(Fname);
+
+var Fname = "Alpesh";
+console.log(Fname);
+ 
+// 2.let keyword
+let fName = "Dipak";
+console.log(fName);
+
+fName = "ishawar";
+console.log(fName);
+fName = "yogesh";
+console.log(fName);
+
+// 3.const keyword
+const num = 20;
+console.log(num);
+
+num = 22;
+console.log(num);
