d0be41d1f8f8f7281f0f91218aff77b8b183ac33
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-09 13:56:07 +0530

    [ADDED]:Dipak - This program will be primitive data type

---
+++ Primitive-Data-Type.js
@@ -0,0 +1,10 @@
+// Primitive data type
+// Number,String,Boolean,Undefined,Null,Bigint,Symbol.
+
+let num = 22;
+let fName = "Hinata"
+isFollow = true; //true&false
+let x;
+let y = null;
+let a = BigInt("12345");
+let b = Symbol("just kidding")
