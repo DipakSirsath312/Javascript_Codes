62bb1c8e05a1370c3ed70f5aec794f4c146dfc42
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-09 13:51:14 +0530

    [ADDED]:Dipak - This is the Practice and create key value pair

---
+++ Practice_1.js
@@ -0,0 +1,11 @@
+const product = {
+    title : "Ball pen",
+    rating : 4,
+    offer : 5,
+    price : 250,
+};
+console.log(product);
+console.log(product["title"])
+console.log(product["rating"])
+console.log(product["offer"])
+console.log(product["price"])
