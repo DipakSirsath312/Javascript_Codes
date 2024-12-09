ea19503c68c5d244ae08026883fbc3660e32142e
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-09 14:00:21 +0530

    [ADDED]:Dipak - This program will be Assignment operators

---
+++ Assignment_operators.js
@@ -0,0 +1,20 @@
+// Assignment Operators
+// =, +=, -=, *=, %=, **=
+
+let a = 10;
+
+a += 1; //a = a + 1;
+console.log(a);    
+
+a -= 1; //a = a - 1;
+console.log(a);
+
+a *= 2; //a = a * 2;
+console.log(a);
+
+a %= 3; //a = a % 3;
+console.log(a);
+
+a **= 2; //a = a ** 2;
+console.log(a);
+
