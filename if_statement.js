0a9c9fc16ffc4fa945fea187b9257e70d22c7879
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-10 14:14:56 +0530

    [ADDED]:Dipak - if statement program you can check condition

---
+++ if_statement.js
@@ -0,0 +1,20 @@
+// Condition Statement
+let age = 23;
+
+if(age >= 18){
+    console.log("You can vote");
+}
+if(age < 18){
+    console.log("You can not vote");
+}
+
+let mode = "dark";
+let color;
+
+if(mode === "dark"){
+    color = "black";
+}
+if(mode === "light"){
+    color = "white";
+}
+console.log(color);
