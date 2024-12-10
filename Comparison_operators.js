5a7645847475794c7bb8f9bfc8802f05d8494e3f
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-10 14:09:56 +0530

    [ADDED]:Dipak - This program will be comparison operators

---
+++ Comparison_operators.js
@@ -0,0 +1,14 @@
+// Comparison_operators
+
+let a = 5;
+let b = 2;
+console.log("5 == 2 :-",a == b);
+console.log("5 != 2 :-",a != b);
+console.log("5 === 2 :-",a === b);
+console.log("5 !== 2 :-", a !== b);
+
+// >,>=,<,<=
+console.log("5 > 2",a > b);
+console.log("5 >= 2 :-",a >= 2);
+console.log("5 < 2 :-",a < b);
+console.log("5 <= 2 :-",a <= b);
