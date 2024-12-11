c2c4262d3cd460bc7aa049cb5e11644c6b5f7910
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-11 12:59:27 +0530

    [ADDED]:Dipak - This Program will be if-else Program condition check

---
+++ if-else_statement.js
@@ -0,0 +1,26 @@
+// if-else statement.
+//1.
+let mode = "light";
+let color;
+if(mode === "dark"){
+    color = "black";
+}else{
+    color = "white";
+}
+console.log(color);
+
+//2.
+let age = 17;
+if(age >= 18){
+    console.log("it's ok")
+}else{
+    console.log("Waiting...");
+}
+//3.
+// Even or Odd 
+let num = 1;
+if(num%2 === 0){
+    console.log(num,"Even Number");
+}else{
+    console.log(num,"Odd Number");
+}
