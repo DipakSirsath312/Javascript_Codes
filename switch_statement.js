9c15d67145b5f7dd058fa9ec7b1bacdca04b2e4b
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-11 13:05:44 +0530

    [ADDED]:Dipak - This program will be switch condition

---
+++ switch_statement.js
@@ -0,0 +1,30 @@
+// switch statement.
+let mode = "dark grey";
+switch(mode){
+    case  "light":
+        console.log("white");
+        break;
+
+    case "pink":
+        console.log("pink");
+        break;
+    
+    case "black":
+        console.log("black");
+        break;
+
+    case "blue":
+        console.log("blue");
+        break;
+
+    case "dark grey":
+        console.log("Dark Grey");
+        break;
+
+    case "skylight":
+        console.log("skyblue");
+        break;
+        
+    default:
+        console.log("Faild");
+}
