ded5474fb396265e5ac2d89dfcb523294991877b
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-12 16:33:29 +0530

    [ADDED]:Dipak - check if the number is a multiple of 5 or not.

---
+++ Practice_set2.js
@@ -0,0 +1,10 @@
+//Let's Practice.
+//1.Get user to input a number using prompt("Enter a number:").check if the number is a multiple of 5 or not.
+
+let num = prompt("Enter a number:-");
+
+if(num % 5 === 0){
+    console.log("5 Number is multiple",num)
+}else{
+    console.log("5 Number is not multiple by 5");
+}
