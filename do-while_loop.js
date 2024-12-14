9c7ec56797b0f04fff004be48ca07f7b386defae
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-14 20:00:54 +0530

    [ADDED]:Dipak - This program will be do-while loop

---
+++ do-while_loop.js
@@ -0,0 +1,17 @@
+//do-while loop.
+let i = 1;
+do{
+    console.log("madara uchiha");
+    i++;
+}while(i <= 10);
+
+// Q2.
+let test = 1;
+do{
+    console.log(test);
+    test++;
+}while(test < 1);
+
+while(test < 1){
+    console.log(test);
+}
