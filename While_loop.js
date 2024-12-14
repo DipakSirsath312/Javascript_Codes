92b8c779e7867bed2d32bdb672682b570e5a1fa3
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-14 19:59:29 +0530

    [ADDED]:Dipak - This Progrram will be while loop

---
+++ While_loop.js
@@ -0,0 +1,21 @@
+// While loop.
+// Q1.
+let i = 1;
+while(i <= 10){
+    console.log("i  =",i);
+    i++;
+}
+// Q2.
+let count = 1;
+let x = 5;
+while(count <= 10){
+    console.log(x * count);
+    count++;
+}
+// Q3.while loop to find traverse an Array.
+let j = 0;
+let arr = [10,20,30,40,50];
+while(j < arr.length){
+    console.log(arr[j]);
+    j++;
+}
