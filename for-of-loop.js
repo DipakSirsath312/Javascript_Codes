635985cba13e76fab17ec35f2d056104c74181ab
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-15 21:26:55 +0530

    [ADDED]:Dipak - This program will be for-of loop use array and string

---
+++ for-of-loop.js
@@ -0,0 +1,15 @@
+// for-of loop.
+// array
+let arr = [10,20,30,40,50];
+for(let i of arr){
+    console.log(i);
+}
+// string
+let str = "dipaksirsath312@gmail.com";
+let size = 0;
+for(let val of str){
+    console.log(val);
+    size++; 
+}  
+console.log("string size:-",size);
+ 
