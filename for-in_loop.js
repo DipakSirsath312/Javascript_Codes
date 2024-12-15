7703f0767f3ed7147d17d320a167b74277ae3fcb
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-15 21:28:59 +0530

    [ADDED]:Dipak - This program will be for-in loop statement

---
+++ for-in_loop.js
@@ -0,0 +1,11 @@
+// for-in loop.
+let student = {
+    Name : "DipakSirsath",
+    age : 22,
+    id : 3766161,
+    number : 8305783244,
+    Add : "khetiya"
+};
+for(let key in student){
+    console.log("Key:-",key, "value:-",student[key]);
+}
