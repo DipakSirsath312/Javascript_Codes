62658be3e948ec61568a3edf6668baa08e451bf7
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-16 11:46:59 +0530

    [ADDED]:Dipak - String is a sequence of character used to represent text

---
+++ string.js
@@ -0,0 +1,11 @@
+// String is a sequence of character used to represent text.
+// create string.
+let str = "DiapkSirsath";
+console.log(str);
+
+// string length.
+console.log(str.length);
+
+// stirng indices.
+console.log(str[0]);
+console.log(str[5]);
