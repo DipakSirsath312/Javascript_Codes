461aa0574bb34c4500c9a07afe7b74d1bf3bc0cb
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-24 21:51:06 +0530

    [ADDED]:Dipak - Arrow function.Compact way of writing a function

---
+++ Arrow_function.js
@@ -0,0 +1,17 @@
+// Arrow function.
+// Compact way of writing a function.
+// sum arrow function.
+const Arrowsum = (a,b) => {
+    return a + b;
+}
+console.log(Arrowsum(34,56));
+// multiplication arrow function.
+const Arrowmul = (x,y) => {
+    return x * y;
+}
+console.log(Arrowmul(2,5));
+
+let Msg = () => {
+    console.log("Hello world");
+}
+Msg();
