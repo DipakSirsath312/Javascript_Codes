8cdc708bfeec982f2677114019cec44f050dad68
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-12-28 19:06:24 +0530

    [ADDED]:Dipak - This program will be Dates function in javascript

---
+++ Dates_in_js.js
@@ -0,0 +1,11 @@
+// Dates in JS.
+let myDate = new Date();
+
+console.log(myDate);
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getFullYear());
+console.log(myDate.getTime());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
+console.log(myDate.getMonth());
